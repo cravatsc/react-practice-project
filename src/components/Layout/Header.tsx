68bf1b2f6b904dd03a113b195c0240ea2c1b7a2e
Craig Cravath <craig.cravath@willowtreeapps.com>
2022-01-27 14:57:43 -0500

    extract header cart button

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,5 +1,19 @@
+import image from '../../assets/meals.jpeg';
+import classes from './Header.module.css';
+import { HeaderCartButton } from './HeaderCartButton';
+
 interface HeaderProps {}
 
 export const Header: React.FC<HeaderProps> = (props: HeaderProps) => {
-  return <></>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={image} alt="A Table full of food" />
+      </div>
+    </>
+  );
 };
